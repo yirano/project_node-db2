2fec13a4ba24a1b8e622f22132aa8bf6bd59eef6
yirano <yirano@protonmail.ch>
2020-07-24 01:15:43 -0400

    Set up seeds and make a get request to test

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const PORT = process.env.PORT || 4000
 
 server.use(express.json())
 
+server.get('/cars', async (req, res, next) => {
+  try {
+    const cars = await db('car-dealer')
+    res.json(cars)
+  } catch (error) {
+    next(error)
+  }
+})
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({ errorMessage: "something happened" })
+})
+
 server.listen(PORT, console.log(`Server running on PORT ${PORT}`))
 
 
--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,12 @@ module.exports = {
   connection: {
     filename: './data/car-dealer.db3'
   },
+  migration: {
+    directory: './data/migrations'
+  },
+  seeds: {
+    directory: './data/seeds'
+  },
   useNullAsDefault: true
 
 }
