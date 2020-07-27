d03f7813bfb09ed73cc9bdc4841f9563b1e2faa6
yirano <yirano@protonmail.ch>
2020-07-26 23:06:04 -0400

    Return updated list on delete and edit operations

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const PORT = process.env.PORT || 4000
 
 server.use(express.json())
 
+const updateList = () => {
+  return async (req, res, next) => {
+    try {
+      const updatedList = await db('car-dealer')
+      res.json(updatedList)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
 server.get('/cars', async (req, res, next) => {
   try {
     const cars = await db('car-dealer')
@@ -15,45 +26,43 @@ server.get('/cars', async (req, res, next) => {
   }
 })
 
-server.get('/cars/:id', async(req, res, next) => {
+server.get('/cars/:id', async (req, res, next) => {
   try {
     const car = await db('car-dealer')
-    .where({'id': req.params.id})
+      .where({ 'id': req.params.id })
     res.json(car)
   } catch (error) {
     next(error)
   }
 })
 
-server.post('/cars', async(req, res, next) => {
+server.post('/cars', async (req, res, next) => {
   try {
     const newCar = await db('car-dealer')
-    .insert(req.body)
+      .insert(req.body)
     res.status(201).json(newCar)
-  } catch (error) {
-   next(error) 
-  }
-})
-
-server.put('/cars/:id', async (req, res, next) => {
-  try {
-     await db('car-dealer')
-    .update(req.body)
-    .where({id: req.params.id})
-
-    const updatedList = await db('car-dealer')
-    res.status(201).json(updatedList)
-
   } catch (error) {
     next(error)
   }
 })
 
-server.delete('/cars/:id', async(req, res, next) => {
+server.put('/cars/:id', async (req, res, next) => {
   try {
-     await db('car-dealer')
-    .where({id: req.params.id})
-    .del()
+    await db('car-dealer')
+      .update(req.body)
+      .where({ id: req.params.id })
+    const updatedList = await db('car-dealer')
+    res.json(updatedList)
+  } catch (error) {
+    next(error)
+  }
+})
+
+server.delete('/cars/:id', async (req, res, next) => {
+  try {
+    await db('car-dealer')
+      .where({ id: req.params.id })
+      .del()
 
     const updatedList = await db('car-dealer')
     res.json(updatedList)
@@ -67,6 +76,9 @@ server.use((err, req, res, next) => {
   console.log(err)
   res.status(500).json({ errorMessage: "something happened" })
 })
+
+
+
 
 server.listen(PORT, console.log(`Server running on PORT ${PORT}`))
 
